a661db6785e4b1d9e85351a24f2422a9ca81a6dc
mgarrett1515 <mgarrett12@cub.uca.edu>
2025-09-22 01:18:22 -0500

    Update fade_in_fade_out.py

--- fade_in_fade_out.py
+++ fade_in_fade_out.py
@@ -1,7 +1,23 @@
-from machine import Pin, PWM, Timer
-from time import sleep
+from machine import Pin, PWM
+import time
 
 
 # SETUP
+led = PWM(Pin(15))   
+led.freq(1000)       
+MAX_DUTY = 65535
+
 
 # LOOP
+while True:
+    
+    for duty in range(0, MAX_DUTY, 650):   
+        led.duty_u16(duty)
+        time.sleep(0.02)  
+
+    
+    for duty in range(MAX_DUTY, -1, -650): 
+        led.duty_u16(duty)
+        time.sleep(0.01)  
+
+#Used AI to help code this 
